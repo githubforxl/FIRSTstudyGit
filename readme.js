25267f1750cf1e46561073e7ca90b5a6abe60213
you@example.com <you@example.com>
2017-04-21 16:42:49 +0800

    v1.0

---
+++ readme.js
@@ -0,0 +1,8 @@
+1.在githup上建一个远程项目仓库；
+2.在本地建一个文件夹，打开git bash here用git clone +仓库的url地址
+  将远程仓库克隆到本地
+3.用git status查询此时代码修改状态；
+4.用git checkout -b +（你自己的分支名字）创建属于你自己的分支；
+5.代码写好后，用git add.将代码存入缓存区；
+6.用git commit -m +（你此次上传的版本名字）提交代码；
+7.用git push将代码上传到远程服务器上；
